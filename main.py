9600c1d4eaa92bc4d955346916155084628a36de
Space2Walker <21970520+Space2Walker@users.noreply.github.com>
2020-10-17 20:42:58 +0200

    German translation & new Artwork

--- main.py
+++ main.py
@@ -1,116 +1,100 @@
 # -*- coding: utf-8 -*-
 
+#  DNS Leak-Test a Kodi addon too check your DNS Settings
+#  Copyright (C) 2020  Space2Walker
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import json
 import sys
 import requests
 import xbmcgui
+import xbmcaddon
 import xbmcplugin
-
+import xbmcvfs
+import urllib.parse as urlparse
 from platform import system as system_name
 from random import randint
 from subprocess import call as system_call
 
-try:
-	import urllib2 as urllib
-	from urllib2 import urlparse
-except ImportError:
-	import urllib.request as urllib
-	import urllib.parse as urlparse
-
-_url = sys.argv[0]
-
-_handle = int(sys.argv[1])
-
 
 def ping(host):
-	param = '-n' if system_name().lower() == 'windows' else '-c'
-	command = ['ping', param, '1', host]
-	if system_name().lower() == 'windows':
-		retcode = system_call(command, creationflags=0x00000008)
-		return retcode == 0
-	else:
-		retcode = system_call(command)
-
-
-def get_url(_url, **kwargs):
-	return '{0}?{1}'.format(_url, urllib.urlencode(kwargs))
+    param = "-n" if system_name().lower() == "windows" else "-c"
+    command = ["ping", param, "1", host]
+    if system_name().lower() == "windows":
+        system_call(command, creationflags=0x00000008)
+    else:
+        system_call(command)
 
 
 def set_label(label):
-	list_item = xbmcgui.ListItem(label=label)
-	list_item.setProperty('IsPlayable', 'false')
-	xbmcplugin.addDirectoryItem(_handle, '', list_item, False)
+    list_item = xbmcgui.ListItem(label=label)
+    list_item.setArt({"fanart": background})
+    xbmcplugin.addDirectoryItem(_handle, "", list_item, False)
 
 
 def list_data(parsed_data, d_type):
-	for dns_server in parsed_data:
-		if dns_server['type'] == d_type:
-			if dns_server['country_name']:
-				if dns_server['asn']:
-					set_label(dns_server['ip']
-							  + " [" + dns_server['country_name']
-							  + ", " + dns_server['asn']
-							  + "]")
-
-				else:
-					set_label(dns_server['ip']
-							  + " [" + dns_server['country_name']
-							  + "]")
-
-			else:
-				set_label(str(dns_server['ip']))
+    for dns_server in parsed_data:
+        if dns_server["type"] == d_type:
+            if dns_server["country_name"]:
+                if dns_server["asn"]:
+                    set_label(
+                        f"{dns_server['ip']} [{dns_server['country_name']}, {dns_server['asn']}]"
+                    )
+                else:
+                    set_label(f"{dns_server['ip']} [{dns_server['country_name']}]")
+            else:
+                set_label(str(dns_server["ip"]))
 
 
-if __name__ == '__main__':
+_handle = int(sys.argv[1])
+my_path = xbmcaddon.Addon().getAddonInfo("path")
+background = xbmcvfs.makeLegalFilename(my_path + "/resources/fanart.png")
+paramstring = sys.argv[2][1:]
+params = dict(urlparse.parse_qsl(paramstring))
+get_local = xbmcaddon.Addon().getLocalizedString
 
-	paramstring = sys.argv[2][1:]
-	params = dict(urlparse.parse_qsl(paramstring))
+if params == {}:
+    xbmcplugin.setContent(_handle, "files")
+    xbmcplugin.setPluginFanart(_handle, background)
+    leak_id = randint(1000000, 9999999)
+    for x in range(0, 10):
+        ping(".".join([str(x), str(leak_id), "bash.ws"]))
 
-	#################################
-	#           1st Start           #
-	#################################
-	if params == {}:
-		xbmcplugin.setPluginCategory(_handle, 'category')
-		xbmcplugin.setContent(_handle, 'videos')
+    url = f"https://bash.ws/dnsleak/test/{str(leak_id)}?json"
+    response = requests.get(url)
+    parsed_data = json.loads(response.content)
+    print(parsed_data)
+    set_label(get_local(62001))
+    list_data(parsed_data, "ip")
+    servers = 0
+    for dns_server in parsed_data:
+        if dns_server["type"] == "dns":
+            servers += 1
+    if servers == 0:
+        set_label(get_local(62002))
+    else:
+        if servers == 1:
+            set_label(get_local(62003))
+        else:
+            set_label(get_local(62004))
+        list_data(parsed_data, "dns")
 
-		leak_id = randint(1000000, 9999999)
-		for x in range(0, 10):
-			ping('.'.join([str(x), str(leak_id), "bash.ws"]))
-
-		url = "https://bash.ws/dnsleak/test/" + str(leak_id) + "?json"
-		response = requests.get(url)
-		parsed_data = json.loads(response.content)
-
-		set_label('Your IP:')
-		list_data(parsed_data, "ip")
-
-		servers = 0
-
-		for dns_server in parsed_data:
-			if dns_server['type'] == "dns":
-				servers += 1
-
-		if servers == 0:
-			set_label("No DNS servers found")
-
-		else:
-			set_label("You use " + str(servers) + " DNS servers:")
-			list_data(parsed_data, "dns")
-
-		set_label("Conclusion:")
-
-		for dns_server in parsed_data:
-			if dns_server['type'] == "conclusion":
-				if dns_server['ip']:
-					set_label(str(dns_server['ip']))
-
-		xbmcplugin.endOfDirectory(_handle)
-		quit()
-
-	#################################
-	#             error             #
-	#################################
-	# If the provided paramstring does not contain a supported action
-	# we raise an exception. This helps to catch coding errors,
-	# e.g. typos in action names.
-	raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+    set_label(get_local(62005))
+    for dns_server in parsed_data:
+        if dns_server["type"] == "conclusion":
+            if dns_server["ip"] == "DNS may be leaking.":
+                set_label(get_local(62006))
+            elif dns_server["ip"] == "DNS is not leaking.":
+                set_label(get_local(62007))
+    xbmcplugin.endOfDirectory(_handle, cacheToDisc=False)
